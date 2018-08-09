b314e3d712167ac11036df5495317f4551d817a0
HM <hello@hazella.co>
2018-08-08 19:42:58 -0500

    Added clear functionality, cleaned up code and UI

--- app/src/main/java/com/hm/todo/MainActivity.java
+++ app/src/main/java/com/hm/todo/MainActivity.java
@@ -7,10 +7,17 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This is a simple ToDo list that utilizes a ListView and ArrayAdapter.
+ * You can dynamically add items to the ToDo list and also clear all items.
+ * A toast message will appear if the user attempts to clear an empty list.
+ *
+ */
 public class MainActivity extends AppCompatActivity {
 
     ArrayAdapter<String> arrayAdapter;
@@ -32,16 +39,38 @@ public class MainActivity extends AppCompatActivity {
         lv.setAdapter(arrayAdapter);
     }
 
+    /**
+     * Add an item to the list
+     * @param view
+     */
     public void addToDoItem(View view) {
-        // Add ToDo item to list
+        // Get item from textbox
         EditText editText = findViewById(R.id.editText3);
         String todo = editText.getText().toString();
 
+        // Add item to list and clear textbox
         todosList.add(todo);
-
         editText.setText("");
         arrayAdapter.notifyDataSetChanged();
+    }
+
+    /**
+     * Delete all the items in the todo list
+     *
+     * @param view
+     */
+    public void clearAll(View view) {
+        // Check if list is empty and alert user if it is
+        if(todosList.isEmpty()) {
+            Toast toast = Toast.makeText(getApplicationContext(), "The list is empty.", Toast.LENGTH_SHORT);
+            toast.show();
+        } else {
+            // Delete all items from the list and update
+            todosList.clear();
+            arrayAdapter.notifyDataSetChanged();
+        }
 
     }
+
 }
 
